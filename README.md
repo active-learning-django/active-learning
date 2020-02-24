db74b84a511393a6c3904209f104d540993d781a
Nick Kebbas <nkebbas@usfca.edu>
2020-02-23 17:36:24 -0800

    add test images to image_labeling app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+## Active Learning Image Labeling
+
 Django project to implement active labeling
+
+## Database 
+
+This application runs SQLite per the default Django settings
+ 
--- imagelabeling/models.py
+++ imagelabeling/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class ImageLabel(models.Model):
+    # ...
+    def __str__(self):
+        return self.imagelabel_text
