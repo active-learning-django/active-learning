3988725e2c5b21d8342dad8b8da9a31e4dd0cfac
Nick Kebbas <nkebbas@usfca.edu>
2020-02-29 17:09:36 -0800

    voting on image abnormality working

--- imagelabeling/urls.py
+++ imagelabeling/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 
-from .views import HomePageView, CreatePostView, LabelImageView
+from .views import HomePageView, CreatePostView
 
 
 urlpatterns = [
 
     path('', HomePageView.as_view(), name='home'),
     path('post/', CreatePostView.as_view(), name='add_post'),
-    path('label/', views.LabelImageView, name='label_image')
+    path('label/', views.LabelImageView, name='label_image'),
+    path('<int:image_id>/', views.detail, name='detail'),
+    path('<int:image_id>/vote/', views.vote, name='vote'),
 ]
--- imagelabeling/views.py
+++ imagelabeling/views.py
@@ -4,13 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
 
 
 from .forms import ImageLabelForm, ImageLabelSubmitForm
 from .models import ImageLabel
+
+from django.shortcuts import get_object_or_404, render
 
 class HomePageView(ListView):
     model = ImageLabel
@@ -34,5 +37,35 @@ def LabelImageView(request):
     template = loader.get_template('imagelabeling/label_image.html')
     success_url = reverse_lazy('home')
     return HttpResponse(template.render(context, request))
+
+def detail(request, image_id):
+    try:
+        image = ImageLabel.objects.get(pk=image_id)
+    except ImageLabel.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'imagelabeling/detail.html', {'image': image})
+
+# this will just update our database with the user's vote of whether image is normal or abnormal
+# we will envoke this from our form in /label, template label_image
+def vote(request, image_id):
+    image = get_object_or_404(ImageLabel, pk=image_id)
+    choice = request.POST['choice']
+    if choice == "Abnormal":
+        selected_choice = image.abnormal_votes
+        selected_choice += 1
+        image.abnormal_votes = selected_choice
+    elif choice == "Normal":
+        selected_choice = image.normal_votes
+        selected_choice += 1
+        image.normal_votes = selected_choice
+    else:
+        selected_choice = image.unknown_votes
+        selected_choice += 1
+        image.unknown_votes = selected_choice
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    image.save()
+    return HttpResponseRedirect('/label')
 
 
